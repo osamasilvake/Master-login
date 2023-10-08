807a8a41a46149afd904d2ee2ff21c6e3dedd2d6
Muhammad osama <mosama007pak@gmail.com>
2023-10-08 21:52:17 +0500

    feat: Add tailwind

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 const App = () => (
 	<div className="App">
 		<h2>Robot GUI</h2>
-		<h1 className="text-xl font-bold underline">Hello world!</h1>
+		<h1 className="text-xl font-bold underline bg-slate-400">Hello world!</h1>
 	</div>
 );
 export default App;
