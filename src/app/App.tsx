def8c94d928665708513d078dc0d1b105ef14e4f
d506315@burda.com <glpat-NQoSpxzunS4XLdYRcy3G>
2023-10-08 13:10:39 +0200

    feat: Eslint rules

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,6 @@
-const App = () => {
-	return (
-		<div className="App">
-			<h2>Robot GUI</h2>
-		</div>
-	);
-};
+const App = () => (
+	<div className="App">
+		<h2>Robot GUI</h2>
+	</div>
+);
 export default App;
