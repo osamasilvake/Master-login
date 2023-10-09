c55bcb9f8fec19f23282cfbf0c5d1393c6c56104
Muhammad osama <mosama007pak@gmail.com>
2023-10-09 22:25:33 +0500

    style: Update background color

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 const App = () => (
 	<div className="App">
 		<h2>Robot GUI</h2>
-		<h1 className="text-xl font-bold bg-slate-400">Hello world!</h1>
+		<h1 className="text-xl font-bold bg-slate-500">Hello world!</h1>
 	</div>
 );
 export default App;
