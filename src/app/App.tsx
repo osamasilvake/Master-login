58e00f96491b2372149b6945b4558fa253e60e0e
Muhammad osama <mosama007pak@gmail.com>
2023-10-11 19:36:53 +0500

    style: Add background color

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 const App = () => (
 	<div className="App">
-		<h2>Robot GUI</h2>
+		<h2 className=" bg-pink-900">Robot GUI</h2>
 		<h1 className="text-xl font-bold bg-slate-500">Hello world!</h1>
 	</div>
 );
