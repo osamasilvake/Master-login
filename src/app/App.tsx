5089ec467adc8fd415ba29d8541d3d4db71700a4
Muhammad osama <mosama007pak@gmail.com>
2023-10-13 20:58:47 +0500

    Revert "feat:  Add login"
    This reverts commit ce48560b1c1d72c1c89fd15f4a6da2c1574386a6.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
-import Login from './Login';
-
 const App = () => (
-	<div>
-		<Login />
-		{/* <h2 className=" bg-pink-900">Robot GUI</h2> */}
-		{/* <h1 className="text-xl font-bold bg-slate-500 text-cyan-300">Hello world!</h1> */}
+	<div className="App">
+		<h2 className=" bg-pink-900">Robot GUI</h2>
+		<h1 className="text-xl font-bold bg-slate-500 text-cyan-300">Hello world!</h1>
 	</div>
 );
 export default App;
