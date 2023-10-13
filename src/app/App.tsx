ce48560b1c1d72c1c89fd15f4a6da2c1574386a6
Muhammad osama <mosama007pak@gmail.com>
2023-10-13 20:56:48 +0500

    feat:  Add login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
+import Login from './Login';
+
 const App = () => (
-	<div className="App">
-		<h2 className=" bg-pink-900">Robot GUI</h2>
-		<h1 className="text-xl font-bold bg-slate-500 text-cyan-300">Hello world!</h1>
+	<div>
+		<Login />
+		{/* <h2 className=" bg-pink-900">Robot GUI</h2> */}
+		{/* <h1 className="text-xl font-bold bg-slate-500 text-cyan-300">Hello world!</h1> */}
 	</div>
 );
 export default App;
