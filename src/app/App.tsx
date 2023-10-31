b391fe8368aa57e452a8fd6a84ea2fe49835ebf1
Muhammad osama <mosama007pak@gmail.com>
2023-10-31 23:22:16 +0500

    refactor: Code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,7 @@
-// import Login from './Login';
-// import LoginForm from './LoginForm';
-// import Signup from './Signup';
-
 import Dashboard from './dashboard/Dashboard';
 
 const App = () => (
 	<div>
-		{/* <Login /> */}
-		{/* <LoginForm /> */}
-		{/* <Signup /> */}
 		<Dashboard />
 	</div>
 );
