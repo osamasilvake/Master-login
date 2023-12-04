049bc43557c8b114037b9827b482545f5d7bd6e7
Muhammad osama <mosama007pak@gmail.com>
2023-12-04 11:33:44 +0500

    feat: Add code

--- src/app/Signup.tsx
+++ src/app/Signup.tsx
@@ -1,15 +1,15 @@
 import loginimage from './loginimage.png';
 const Signup = () => (
 	<div className="flex flex-col justify-center items-center pt-20 md:items-stretch  md:flex-row">
-		<div className="bg-white flex w-96">
-			<form className="flex flex-col py-16 mx-auto">
+		<div className="bg-white flex w-96 py-10">
+			<form className="flex flex-col py-12 mx-auto">
 				<h2 className="text-center pb-5 text-3xl">Sign up</h2>
 				<p className="text-center pb-6">Enter your credentials to signup</p>
-				<input type="text" placeholder="username" className="rounded-md outline-none px-2 py-1 bg-slate-100" />
+				<input type="text" placeholder="username" className="rounded-md outline-none px-2 py-1 bg-slate-100 w-64" />
 				<input
 					type="password"
 					placeholder="password"
-					className="mt-4 rounded-md outline-none px-2 border-0 py-1 bg-slate-100"
+					className="mt-4 w-64 rounded-md outline-none px-2 border-0 py-1 bg-slate-100"
 				/>
 				<button className="bg-black text-white py-1 mt-4 rounded-md">Signup</button>
 				<p className="text-center pt-7 text-sm">
@@ -21,7 +21,7 @@ const Signup = () => (
 			</form>
 		</div>
 		<div className="w-96">
-			<img src={loginimage} alt={loginimage} />
+			<img src={loginimage} alt={loginimage} className="h-full" />
 		</div>
 	</div>
 );
