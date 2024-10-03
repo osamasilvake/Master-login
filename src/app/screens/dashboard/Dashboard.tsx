1b2b8cebb93333240ceb3ec75fe2927aa6bb80b9
osamasilvake <muhammadosamavu@gmail.com>
2024-10-03 02:44:52 -0700

    refactor: Code

--- src/app/screens/dashboard/Dashboard.tsx
+++ src/app/screens/dashboard/Dashboard.tsx
@@ -1,22 +1,12 @@
-import { useSelector } from 'react-redux';
-
-import { selectAuth } from '@/slices/auth/Auth.slice';
-
 import Card from './Card';
 import RecentFiles from './RecentFiles';
 import SearchInput from './SearchInput';
 
-const Dashboard = () => {
-	const auth = useSelector(selectAuth);
-	// const dispatch = useDispatch<AppDispatch>(); // generic <AppDispatch>
-	console.log(auth);
-
-	return (
-		<main className="bg-101 px-5 flex-1 pt-16 md:pt-5">
-			<SearchInput />
-			<Card />
-			<RecentFiles />
-		</main>
-	);
-};
+const Dashboard = () => (
+	<main className="bg-101 px-5 flex-1 pt-16 md:pt-5">
+		<SearchInput />
+		<Card />
+		<RecentFiles />
+	</main>
+);
 export default Dashboard;
